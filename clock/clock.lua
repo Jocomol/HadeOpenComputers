fdc5447434a35c1db1e73cc6aa8816c7bea70473
Jocomol <joelmeier08@gmail.com>
2020-07-23 16:08:20 +0200

    - clock.lua for a microcontroller

--- clock/clock.lua
+++ clock/clock.lua
@@ -1,41 +1,13 @@
-local cfg = {}
 local component = require("component")
 local sides = require("sides")
-
-print("exit with ctrl+alt+c")
-
-local f = io.open("./clock.cfg","rb")
-if f then
- local newcfg = serial.unserialize(f:read("*a"))
- f:close()
- for key,value in pairs(newcfg) do
-  cfg[key] = value
-end
-
-side = nil
-if (cfg[key]=="up")
-  side = sides.up
-end
-if (cfg[key]=="bottom")
-  side = sides.bottom
-end
-if (cfg[key]=="left")
-  side = sides.left
-end
-if (cfg[key]=="right")
-  side = sides.right
-end
-if (cfg[key]=="front")
-  side = sides.front
-end
-if (cfg[key]=="back")
-  side = sides.back
-end
-
 while (true)
 do
-  os.sleep(cfg[timeOff])
-  component.redstone.setOutput(sides.up, 15)
-  os.sleep(cfg[timeOn])
-  component.redstone.setOutput(sides.up, 0)
+  while (component.redstone.getInput(sides.left) > 0)
+  do
+    os.sleep(0.05)
+    component.redstone.setOutput(sides.right, 15)
+    os.sleep(0.05)
+    component.redstone.setOutput(sides.right, 0)
+  end
+
 end
